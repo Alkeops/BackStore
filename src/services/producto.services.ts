ff3ea07adfa77fc4ced2cd220b3468b70c6224e2
alkeops <m_adan2012@outlook.com>
2021-03-30 20:12:03 -0600

    file delete

--- src/services/producto.services.ts
+++ src/services/producto.services.ts
@@ -3,27 +3,9 @@ import { Producto, DataP, ResponseP } from "@interfaces";
 import { returnForApiProductos } from "@utils";
 import { ProductoModel } from "@databases/models";
 
-const DIRFOLDER = `${__dirname}/productos.txt`;
-
-if (!fs.existsSync(DIRFOLDER)) fs.writeFileSync(DIRFOLDER, JSON.stringify([]));
-
 //TODO Servicios para Productos, probablemente crear una clase abstracta para todos los servicios y extender de ella.
 
 export class ProductosServices {
-  state: Producto[]; //Array de la interface Producto en ts/interfaces
-
-  constructor() {
-    this.state = [];
-    this.init();
-  }
-
-  //Inicializa y llena el array del state con el archivo txt
-  init = () => {
-    const read = fs.readFileSync(DIRFOLDER);
-    const data = JSON.parse(read);
-    return (this.state = data);
-  };
-
   //Devuelve todo lo que halla en el array
   all = async (): Promise<ResponseP> => {
     const respuesta = await ProductoModel.find({});
@@ -53,8 +35,6 @@ export class ProductosServices {
     }
   };
 
-  //Metodo repetido para clase abstracta**
-  //TODO Comprobar si el id existe
   delete = async (id: string): Promise<ResponseP> => {
     try {
       const { deletedCount } = await ProductoModel.deleteOne({ _id: id });
@@ -87,9 +67,5 @@ export class ProductosServices {
     } catch (error) {
       return returnForApiProductos(500, "Error", [error]);
     }
-  };
-
-  rewriteFile = () => {
-    fs.writeFileSync(DIRFOLDER, JSON.stringify(this.state));
   };
 }
